3324365de92dbc52efcbc4efafbe09ed5f91ba37
JuwonCha <juwon1512@gmail.com>
2022-08-03 00:44:14 +0900

    Initial Commit

---
+++ PracticeUnrealEngine/Source/PracticeUnrealEngine/PracticeUnrealEngineGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PracticeUnrealEngineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACTICEUNREALENGINE_API APracticeUnrealEngineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
